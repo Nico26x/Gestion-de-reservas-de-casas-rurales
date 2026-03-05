868a7965cf804c8d51a4e5839bda9d511673182d
Nico26x <nicolas.loaizab@uqvirtual.edu.co>
2026-03-04 19:32:15 -0500

    Base del proyecto en springboot con angular y base de datos mariadb

---
+++ backend/ReservasCasasRurales/src/main/java/com/reservas/ReservasCasasRuralesApplication.java
@@ -0,0 +1,13 @@
+package com.reservas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservasCasasRuralesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservasCasasRuralesApplication.class, args);
+    }
+
+}
