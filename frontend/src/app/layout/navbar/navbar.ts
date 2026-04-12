79444da585963b7af4962c8bac7863786f1abd7b
Nico26x <nicolas.loaizab@uqvirtual.edu.co>
2026-04-12 00:13:38 -0500

    Solucion de error en navbar.ts y diseño de la pagina home

--- frontend/src/app/layout/navbar/navbar.ts
+++ frontend/src/app/layout/navbar/navbar.ts
@@ -1,12 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router, RouterLink } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../core/services/auth/auth.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
