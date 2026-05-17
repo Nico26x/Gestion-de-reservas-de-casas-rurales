0557f8c1c087aa32f5df8df868d1b35fbf114626
JU4NJ053M <juanj.mesao@uqvirtual.edu.co>
2026-05-17 02:15:52 -0500

    Intento 7 deploy (ojalá ultimo jaja)

--- backend/ReservasCasasRurales/src/main/java/com/reservas/config/SecurityConfig.java
+++ backend/ReservasCasasRurales/src/main/java/com/reservas/config/SecurityConfig.java
@@ -99,7 +99,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("https://d2te3u5jwt4877.cloudfront.net"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("Authorization"));
